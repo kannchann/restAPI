e9134660a3598b66c255d632b6122b404b034e46
kannchann <kannchann.thapa@gmail.com>
2024-08-21 15:26:17 +0545

    redirect to home page

--- productDetails.js
+++ productDetails.js
@@ -90,3 +90,9 @@ document.body.addEventListener("click", function (event) {
 });
 
 productDetails();
+
+const backButton = document.querySelector(".back-button");
+
+backButton.addEventListener("click", () => {
+  window.location.href = `index.html`;
+});
